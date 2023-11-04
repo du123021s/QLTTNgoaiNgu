fc9caf331cbb30c986f61d6cc6f070c1fc1b55bf
du123021s <102853848+du123021s@users.noreply.github.com>
2023-11-04 18:28:07 +0700

    Cap Nhat Giao Dien lan 3 - 4/11/2023

--- src/main/java/view/GiangVienUI.java
+++ src/main/java/view/GiangVienUI.java
@@ -17,6 +17,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -28,13 +29,14 @@ public class GiangVienUI extends Application {
     private Scene scene;
     private Stage stage;
 
-    private Button hvBtn, lichHocBtn, taiLieuBtn, khoaHocBtn, dangSuatBtn;
+    private Button hvBtn, lichHocBtn, taiLieuBtn, khoaHocBtn,
+                dangSuatBtn, giangvienBtn, lophocBtn, phonghocBtn, tlkhoahocBtn ;
     private InputStream input;
     private Image img;
     private ImageView imgView;
 
-    private String menuFormat = "-fx-background-color:#005864 ;-fx-text-fill:#FFFFFF;";
-    private String menuEffect = "-fx-background-color:#F7FA5A;-fx-text-fill:#000000;";
+    private String menuFormat = "-fx-background-color:#21618C;-fx-text-fill:#FFFFFF;";
+    private String menuEffect = "-fx-background-color:#F1C40F;-fx-text-fill:#000000;";
     public static void main(String[] agrs){
         launch(agrs);
     }
@@ -58,7 +60,7 @@ public class GiangVienUI extends Application {
         menu.getItems().addAll(item1, item2, item3);
 
 
-        rootBp.setTop(menubar);
+
         menubar.getMenus().add(menu);
 
         // === Thiết lập phần biểu tượng app
@@ -69,38 +71,52 @@ public class GiangVienUI extends Application {
 
         // === Thiết lập layout các chức năng cho Giang viên sử dụng (bên trái app)
         // === Logo icon
-        Text logoTitle = new Text("ABC");
+        Text logoTitle = new Text("BLUEDREAM");
         logoTitle.setFont(Font.font("SansSerif", FontWeight.BOLD, 47));
         HBox logoTitleHb = new HBox(10);
         logoTitleHb.setPadding(new Insets(15,0,15,0));
         logoTitleHb.getChildren().addAll(getlogoIcon(), logoTitle);
         logoTitleHb.getStyleClass().add("logoTitleHb");
 
-        // === Tạo đường kẻ ngang
-        Line horizontalLine = new Line(0, 1, 255, 1);
-        horizontalLine.setStroke(Color.GRAY);
-            /*Tạo một Pane để đặt đường kẻ vào */
-        Pane paneLine = new Pane(horizontalLine);
-        paneLine.setPadding(new Insets(10,0,15,0));
+        VBox banner = new VBox();
+        banner.getChildren().addAll(menubar, logoTitleHb);
+
 
         // === Chức năng
+        Text titleQuanLy = new Text("QUẢN LÝ TRUNG TÂM");
+        HBox titleQuanLyHb = new HBox();
+        titleQuanLyHb.setPadding(new Insets(25));
+        titleQuanLyHb.getStyleClass().add("titleQuanLy");
+        titleQuanLyHb.getChildren().add(titleQuanLy);
+
         hvBtn = new Button("Quản Lý Học Viên");
-        khoaHocBtn = new Button("Xem Khóa Học");
-        lichHocBtn = new Button("Xem Lịch Dạy");
-        taiLieuBtn = new Button("Xem Tài Liệu");
-        dangSuatBtn = new Button("Đăng Suất");
+        giangvienBtn = new Button("Quản Lý Giảng Viên");
+        khoaHocBtn = new Button("Quản Lý Khóa Học");
+        phonghocBtn = new Button("Quản Lý Phòng Học");
+        lophocBtn = new Button("Quản Lý Lớp Học");
+        lichHocBtn = new Button("Sắp Xếp Lịch Học");
+        taiLieuBtn = new Button("Quản Lý Tài Liệu");
+        tlkhoahocBtn = new Button("Tài Liệu Cho Khóa Học");
+
 
         hvBtn.setGraphic(getStudentIcon()); hvBtn.setAlignment(Pos.CENTER_LEFT);
         khoaHocBtn.setGraphic(getCourseIcon()); khoaHocBtn.setAlignment(Pos.CENTER_LEFT);
         lichHocBtn.setGraphic(getTimeTableIcon()); lichHocBtn.setAlignment(Pos.CENTER_LEFT);
         taiLieuBtn.setGraphic(getFileIcon()); taiLieuBtn.setAlignment(Pos.CENTER_LEFT);
-        dangSuatBtn.setGraphic(getLogoutIcon()); dangSuatBtn.setAlignment(Pos.CENTER_LEFT);
+        giangvienBtn.setGraphic(getLogoutIcon()); giangvienBtn.setAlignment(Pos.CENTER_LEFT);
+        phonghocBtn.setGraphic(getLogoutIcon()); phonghocBtn.setAlignment(Pos.CENTER_LEFT);
+        lophocBtn.setGraphic(getLogoutIcon()); lophocBtn.setAlignment(Pos.CENTER_LEFT);
+        tlkhoahocBtn.setGraphic(getStudentIcon()); tlkhoahocBtn.setAlignment(Pos.CENTER_LEFT);
 
         hvBtn.setStyle(menuFormat);
         khoaHocBtn.setStyle(menuFormat);
         lichHocBtn.setStyle(menuFormat);
         taiLieuBtn.setStyle(menuFormat);
-        dangSuatBtn.setStyle(menuFormat);
+        giangvienBtn.setStyle(menuFormat);
+        phonghocBtn.setStyle(menuFormat);
+        lophocBtn.setStyle(menuFormat);
+        tlkhoahocBtn.setStyle(menuFormat);
+
         // === Thiếp lập nội dung cụ thể cho từng chức năng (bên phải app)
 
         VBox leftVb = new VBox(10);
@@ -113,10 +129,17 @@ public class GiangVienUI extends Application {
         khoaHocBtn.prefWidthProperty().bind(leftVb.widthProperty());
         lichHocBtn.prefWidthProperty().bind(leftVb.widthProperty());
         taiLieuBtn.prefWidthProperty().bind(leftVb.widthProperty());
-        dangSuatBtn.prefWidthProperty().bind(leftVb.widthProperty());
+        giangvienBtn.prefWidthProperty().bind(leftVb.widthProperty());
+        phonghocBtn.prefWidthProperty().bind(leftVb.widthProperty());
+        lophocBtn.prefWidthProperty().bind(leftVb.widthProperty());
+        tlkhoahocBtn.prefWidthProperty().bind(leftVb.widthProperty());
 
-        leftVb.getChildren().addAll(logoTitleHb, paneLine,  hvBtn, khoaHocBtn, lichHocBtn, taiLieuBtn, dangSuatBtn);
+        leftVb.getChildren().addAll(titleQuanLyHb, hvBtn, giangvienBtn, khoaHocBtn,phonghocBtn, lophocBtn, lichHocBtn, taiLieuBtn, tlkhoahocBtn);
 
+//        BorderPane borderPane = new BorderPane();
+//        borderPane.setLeft(leftVb);
+
+        rootBp.setTop(banner);
         rootBp.setLeft(leftVb);
         scene = new Scene(rootBp, 900, 750);
         scene.getStylesheets().add(getClass().getResource("/css/GiangVienUI.css").toExternalForm());
@@ -158,15 +181,38 @@ public class GiangVienUI extends Application {
             taiLieuBtn.setStyle(menuFormat);
         });
 
-        dangSuatBtn.setOnMouseEntered((event) -> {
-            dangSuatBtn.setStyle(menuEffect);
+        giangvienBtn.setOnMouseEntered((event) -> {
+            giangvienBtn.setStyle(menuEffect);
         });
 
-        dangSuatBtn.setOnMouseExited((event) -> {
-            dangSuatBtn.setStyle(menuFormat);
+       giangvienBtn.setOnMouseExited((event) -> {
+            giangvienBtn.setStyle(menuFormat);
+        });
+        phonghocBtn.setOnMouseEntered((event) -> {
+            phonghocBtn.setStyle(menuEffect);
+        });
+
+        phonghocBtn.setOnMouseExited((event) -> {
+            phonghocBtn.setStyle(menuFormat);
+        });
+        lophocBtn.setOnMouseEntered((event) -> {
+            lophocBtn.setStyle(menuEffect);
+        });
+
+        lophocBtn.setOnMouseExited((event) -> {
+            lophocBtn.setStyle(menuFormat);
+        });
+        tlkhoahocBtn.setOnMouseEntered((event) -> {
+            tlkhoahocBtn.setStyle(menuEffect);
+        });
+
+        tlkhoahocBtn.setOnMouseExited((event) -> {
+            tlkhoahocBtn.setStyle(menuFormat);
         });
     }
 
+
+// ============ HÀM LẤY ẢNH ========================================================
     public ImageView getlogoIcon(){
         input = getClass().getResourceAsStream("/icons/logoRg3.png");
         img = new Image(input);
